15f06ce93f79d86122ab642cef6312ecc05dfd39
Chris Anders <filmee24@gmail.com>
2026-01-27 16:26:08 +0100

    feat: Add Abstraction For Native Function Definitions

---
+++ src/ObjectModel/Evaluation/Function.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectModel.Evaluation;
+
+internal interface IFunction : IEvaluable
+{
+    string Name { get; }
+    List<string> Parameters { get; }
+
+    object Invoke(params object[] parameters);
+}
+
+
+internal abstract class Function : IFunction
+{
+    public abstract string Name { get; }
+    public abstract List<string> Parameters { get; }
+
+    public object Evaluate(Evaluator evaluator, Scope scope)
+    {
+        var arguments = Parameters.Select(p => scope.GetValue(p)).ToArray();
+        return Invoke(arguments);
+    }
+
+    public abstract object Invoke(params object[] parameters);
+}
+
+internal abstract class Function<T1, T2, TResult> : Function
+{
+    public override object Invoke(params object[] parameters)
+    {
+        if (parameters.Length != 2)
+            throw new System.ArgumentException($"Function {Name} expects 2 parameters.");
+
+        return Invoke((T1)parameters[0], (T2)parameters[1]);
+    }
+
+    public abstract TResult Invoke(T1 param1, T2 param2);
+}
+
+internal abstract class Function<T1, TResult> : Function
+{
+    public override object Invoke(params object[] parameters)
+    {
+        if (parameters.Length != 2)
+            throw new System.ArgumentException($"Function {Name} expects 2 parameters.");
+
+        return Invoke((T1)parameters[0]);
+    }
+
+    public abstract TResult Invoke(T1 param1);
+}
