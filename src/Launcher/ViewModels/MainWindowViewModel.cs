1f7640a7b483fbba7df49d8598730af4d8dc6623
Chris Anders <filmee24@gmail.com>
2026-02-07 13:35:30 +0100

    feat(launcher): Add Changelog

--- src/Launcher/ViewModels/MainWindowViewModel.cs
+++ src/Launcher/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Octokit;
 using Velopack;
 using Velopack.Sources;
+using ProductHeaderValue = Octokit.ProductHeaderValue;
 
 namespace BrineAndCoin.Launcher.ViewModels;
 
@@ -16,11 +20,22 @@ public partial class MainWindowViewModel : ObservableObject
     [ObservableProperty]
     private string _changelog = string.Empty;
 
+    public MainWindowViewModel()
+    {
+        var client = new GitHubClient(new ProductHeaderValue("Brine-and-Coin-Launcher"));
+
+        var repoOwner = "Portraits-in-Brick-and-Time";
+        var repoName = "Brine-and-Coin";
+
+        var latestRelease = client.Repository.Release.GetLatest(repoOwner, repoName).Result;
+        Changelog = latestRelease.Body;
+    }
+
     [RelayCommand]
     async Task CheckForUpdatesAsync()
     {
 #if DEBUG
-        return;
+        goto start;
 #endif
 
         var mgr = new UpdateManager(new GithubSource("https://github.com/Portraits-in-Brick-and-Time/Brine-and-Coin", null, false));
@@ -31,8 +46,7 @@ public partial class MainWindowViewModel : ObservableObject
             goto start;
         }
 
-        Changelog = newVersion.TargetFullRelease.NotesMarkdown;
-                await mgr.DownloadUpdatesAsync(newVersion, progress =>
+        await mgr.DownloadUpdatesAsync(newVersion, progress =>
         {
             ProgressValue = progress;
         });
@@ -40,7 +54,24 @@ public partial class MainWindowViewModel : ObservableObject
         mgr.ApplyUpdatesAndRestart(newVersion);
 
 start:
-        Process.Start("Portraits_in_Brick_and_Time_-_Brine_And_Coin-Game.exe");
-        Environment.Exit(0);
+      StartGame();
+      Environment.Exit(0);
+    }
+
+    private void StartGame()
+    {
+        const string assemblyName = "Portraits_in_Brick_and_Time_-_Brine_And_Coin-Game";
+
+        var fileToRun = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? $"{assemblyName}.exe"
+            : assemblyName;
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = fileToRun,
+            UseShellExecute = false
+        };
+
+        Process.Start(psi);
     }
 }
